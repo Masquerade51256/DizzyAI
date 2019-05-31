e0d2f2d30fd68d5ed9c282d9a44237cd3936418e
Fancy-Block <crstina1113@sina.com>
2019-05-31 11:31:14 +0800

    修bug
    解决“请一天假”的问题

--- get_reason.py
+++ get_reason.py
@@ -90,7 +90,7 @@ def find_reason(trees):
     vp = ""
     final_result = []
     for tree in trees:
-        # tree.pretty_print()
+        tree.pretty_print()
         sentence = "".join(tree.leaves())
         if contain_approver(tree):
             # trees.remove(tree)
@@ -101,6 +101,11 @@ def find_reason(trees):
         # pos, _ = tn.parse(sentence)
         # if len(pos) > 0:
         #     print(pos)
+        #     print(pos[0])
+        #     print(pos[0][0])
+        #     print(pos[0][1])
+        #     print(sentence[pos[0][0]])
+            # print(sentence.type())
         if re.match(r'(.*)请(.*?)假(.*).*', sentence) is not None:
             # 判断是否有其他动词
             current_tree = tree
@@ -139,21 +144,45 @@ def preprocess(sentence):
     return sentence
 
 
-# def main():
-#     with StanfordCoreNLP(r'D:\corenlp\stanford-corenlp-full-2018-10-05', lang='zh', memory='4g', quiet=True,) as nlp:
-#         nlp.parse("test")
-#         while True:
-#             print("请输入")
-#             sentence = input()
-#             sentence = preprocess(sentence)
-#             splits = re.compile("[,，。,]").split(sentence)
-#             results = [nlp.parse(s) for s in splits]
-#             trees = [ParentedTree.fromstring(result) for result in results]
-#             final_result = find_reason(trees)
-#             output = "".join(final_result)
-#             print(len(output))
-#             print(output)
-#
-#
-# main()
+def main():
+    with StanfordCoreNLP(r'D:\corenlp\stanford-corenlp-full-2018-10-05', lang='zh', memory='4g', quiet=True,) as nlp:
+        nlp.parse("test")
+        while True:
+            print("请输入")
+            sentence = input()
+            sentence = preprocess(sentence)
+            tn = TimeNormalizer()
+            pos, _ = tn.parse(sentence)
+            if len(pos) > 0:
+                print("pos tuple", pos)
+                print("length of pos", len(pos))
+                # new_str = ""
+                for i in range(len(pos)-1, -1, -1):
+                    for j in range(pos[i][1]-1, pos[i][0]-1, -1):
+                        print("pos", j)
+                        print("word", sentence[j])
+                        sentence = sentence[:j]+sentence[j+1:]
+                        print("after", sentence)
+                print(sentence)
+            print(sentence)
+            splits = re.compile("[,，。,]").split(sentence)
+
+            # for s in splits:
+            #     pos, _ = tn.parse(s)
+            #     if len(pos) > 0:
+            #         new_str = ""
+            #         for i in range(0, len(s)):
+            #             if i < pos[0][0] or i >= pos[0][1]:
+            #                 new_str = new_str + s[i]
+            #         s = new_str
+            #         print(s)
+            results = [nlp.parse(s) for s in splits]
+            trees = [ParentedTree.fromstring(result) for result in results]
+            final_result = find_reason(trees)
+            output = "".join(final_result)
+            print(len(output))
+            print(output)
+
+
+main()
 
