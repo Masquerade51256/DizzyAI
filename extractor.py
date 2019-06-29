a038a5423b46e3f5617968265e316f4e6a7aa42d
lyx <40358376+LyXuAnn@users.noreply.github.com>
2019-06-29 22:05:45 +0800

    EX_001 unit test

--- extractor.py
+++ extractor.py
@@ -29,24 +29,25 @@ class Extractor:
         return
 
     def extract_email(self, text):
-        if text == '':
-            return None
-        eng_texts = self.replace_chinese(text)
-        eng_texts = eng_texts.replace(' at ', '@').replace(' dot ', '.')
-        sep = ',!?:; ，。！？《》、|\\/'
-        eng_split_texts = [''.join(g) for k, g in groupby(eng_texts, sep.__contains__) if not k]
+        try:
+            eng_texts = self.replace_chinese(text)
+            eng_texts = eng_texts.replace(' at ', '@').replace(' dot ', '.')
+            sep = ',!?:; ，。！？《》、|\\/'
+            eng_split_texts = [''.join(g) for k, g in groupby(eng_texts, sep.__contains__) if not k]
 
-        email_pattern = r'^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z_-]+)+$'
+            email_pattern = r'^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z_-]+)+$'
 
-        emails = []
-        for eng_text in eng_split_texts:
-            result = re.match(email_pattern, eng_text, flags=0)
-            if result:
-                emails.append(result.string)
-        if len(emails) == 0:
+            emails = []
+            for eng_text in eng_split_texts:
+                result = re.match(email_pattern, eng_text, flags=0)
+                if result:
+                    emails.append(result.string)
+            if len(emails) == 0:
+                return None
+            else:
+                return emails
+        except:
             return None
-        else:
-            return emails
 
     def replace_chinese(self, text):
         if text == '':
@@ -55,31 +56,15 @@ class Extractor:
         text_without_chinese = filtrate.sub(r' ', text)
         return text_without_chinese
 
-    def replace_ids(self, text):
-        if text == '':
-            return []
-        eng_texts = self.replace_chinese(text)
-        sep = ',!?:; ：，.。！？《》、|\\/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        eng_split_texts = [''.join(g) for k, g in groupby(eng_texts, sep.__contains__) if not k]
-        eng_split_texts_clean = [ele for ele in eng_split_texts if len(ele) == 18]
-
-        id_pattern = r'^[1-9][0-7]\d{4}((19\d{2}(0[13-9]|1[012])(0[1-9]|[12]\d|30))|(19\d{2}(0[13578]|1[02])31)|(19\d{2}02(0[1-9]|1\d|2[0-8]))|(19([13579][26]|[2468][048]|0[48])0229))\d{3}(\d|X|x)?$'
-        ids = []
-        for eng_text in eng_split_texts_clean:
-            result = re.match(id_pattern, eng_text, flags=0)
-            if result:
-                ids.append(result.string)
-
-        for phone_num in ids:
-            text = text.replace(phone_num, '')
-        return text
-
     def extract_name(self, text):
-        result = self.name_match(self.Auditor, text)
-        if result == 0:
+        try:
+            result = self.name_match(self.Auditor, text)
+            if result == 0:
+                return None
+            else:
+                return result
+        except:
             return None
-        else:
-            return result
 
     @staticmethod
     def auditor_transform(chinese_auditor_nameList):
@@ -148,10 +133,3 @@ class Extractor:
                     if distance < len(employer):
                         return auditor[len(employer):]
         return 0
-
-    def most_common(self, content_list):
-        if content_list is []:
-            return None
-        if len(content_list) == 0:
-            return None
-        return max(set(content_list), key=content_list.count)
